4c31bf0a397d2c1713f2f1489026ce8a0df99fea
Nicolas Peniel <135339436+NicoSoft08@users.noreply.github.com>
2025-08-01 17:23:44 +0300

    Delete app directory

--- app/components/posts/PostsList.jsx
+++
@@ -1,9 +0,0 @@
-import '../../styles/posts/PostsList.scss';
-
-export default function PostsList({ children }) {
-    return (
-        <div className='posts-list'>
-            {children}
-        </div>
-    );
-};

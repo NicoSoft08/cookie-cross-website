0c633952f886f508653f59bf75b6f8b134bf212d
Nicolas Peniel <135339436+NicoSoft08@users.noreply.github.com>
2025-08-03 19:57:48 +0300

    Add files via upload

--- id/src/pages/Identity.jsx
+++ id/src/pages/Identity.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
 const AUTH_URL = process.env.REACT_APP_AUTH_URL || 'http://localhost:3001';

1aeb14609dc7a51821f4e48693414f982e36834d
Nicolas Peniel <135339436+NicoSoft08@users.noreply.github.com>
2025-08-23 09:31:43 +0300

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -1,11 +1,8 @@
 const app = require('./src/app');
-
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-    res.send('API is running');
-});
+app.get('/', (_req, res) => res.send('OK')); // ping minimal
 
-app.listen(PORT, async () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`API listening on http://localhost:${PORT}`);
 });
